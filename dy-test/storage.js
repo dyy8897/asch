659a890d36d54bc9a2a27054fe095549f2abfccc
dingyun <dingyun@hitb.com.cn>
2018-01-30 11:38:40 +0800

    添加asch-js测试代码

--- dy-test/storage.js
+++ dy-test/storage.js
@@ -1,2 +1,10 @@
 const AschJS = require('asch-js');
 const Request = require('superagent');
+
+const secret = 'someone manual strong movie roof episode eight spatial brown soldier soup motor';
+const secondSecret = '123456'
+
+const content = new Buffer('helloworld').toString('hex')
+const a = AschJS.storage.createStorage(content, secret, secondSecret)
+console.log('创建存储交易')
+console.log(a)
--- dy-test/transation.js
+++ dy-test/transation.js
@@ -1,2 +1,16 @@
 const AschJS = require('asch-js');
 const Request = require('superagent');
+
+const targetAddress = "16358246403719868041"; 
+const amount = 100*100000000;   //100 XAS
+const message = '备注';
+const secret = 'someone manual strong movie roof episode eight spatial brown soldier soup motor';
+const secondSecret = '123456'
+
+const trans = AschJS.transaction.createTransaction(targetAddress, amount, message, secret, secondSecret)
+console.log('在主链转账')
+console.log(trans)
+
+const trans_id = AschJS.crypto.getId(trans)
+console.log('获取交易ID')
+console.log(trans_id)
