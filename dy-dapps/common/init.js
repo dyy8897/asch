8bf78f2862b75726ac4ca68a69ac9b8919ecea00
dingyun <dingyun@hitb.com.cn>
2018-02-03 17:59:08 +0800

    先把CCtime的代码拷贝过来,学习着改.

--- dy-dapps/common/init.js
+++ dy-dapps/common/init.js
@@ -1,9 +1,20 @@
+const IntervalCache = require('./lib/interval-cache')
+
 module.exports = async function () {
   console.log('enter dapp init')
 
   app.registerContract(1000, 'domain.register')
   app.registerContract(1001, 'domain.set_ip')
 
+  app.registerContract(1000, 'cctime.postArticle')
+  app.registerContract(1001, 'cctime.postComment')
+  app.registerContract(1002, 'cctime.voteArticle')
+  app.registerContract(1003, 'cctime.likeComment')
+  app.registerContract(1004, 'cctime.report')
+
+  app.setDefaultFee('10000000', 'CCTime.XCT')
+
+  app.custom.cache = new IntervalCache(10 * 1000)
 
   app.events.on('newBlock', (block) => {
     console.log('new block received', block.height)
