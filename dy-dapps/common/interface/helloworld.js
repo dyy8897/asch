d455185b05388057e6624c9dbe63b7edc97a167e
dzc944262316 <duanzhichao@hitb.com.cn>
2018-02-06 16:47:38 +0800

    增加调用分组器

--- dy-dapps/common/interface/helloworld.js
+++ dy-dapps/common/interface/helloworld.js
@@ -1,9 +1,21 @@
 const AschJS = require('asch-js');
+const request = require('superagent');
+const async = require('async');
 app.route.get('/hello',  async function (req) {
   const secret = "tourist fabric genius organ inherit soft awkward pool boy police awake genius"
   console.log(secret)
+  console.log('sssssssssssdasdasdasdqweqwrqwefsdfd')
   const message = AschJS.crypto.getKeys(secret)
-  return { message: message }
+  return { message: 'ssssssss' }
+})
+
+app.route.post('/drgserver',  async function (req) {
+  data = JSON.stringify(req.query);
+  var response = await request
+                        .post('127.0.0.1:3002/comp_drg')
+                        .send(req.query)
+                        .set('Content-Type','application/json')
+  return {reports: response["text"]}
 })
 
 app.route.put('/ping/:seq',  async function (req) {
@@ -15,4 +27,11 @@ app.route.post('/sleep',  async function (req, cb) {
     throw new Error('Invalid params')
   }
   await new Promise(resolve => setTimeout(resolve, Math.floor(Number(req.query.seconds) * 1000)))
+  // return { pong: req }
+})
+
+
+app.route.get('/my_height',  async function (req) {
+  let height = await app.model.Comment.findAll({limit: 300, sort: {}})
+  return { message: height}
 })
