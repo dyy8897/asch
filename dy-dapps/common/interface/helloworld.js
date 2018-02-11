6eb7eb87a5b0d276eafeb66decd22228af4a47e8
duanzhichao <duanzhichao2008@gmail.com>
2018-02-11 16:51:25 +0800

    完成调用分组器,产生交易

--- dy-dapps/common/interface/helloworld.js
+++ dy-dapps/common/interface/helloworld.js
@@ -30,25 +30,36 @@ app.route.get('/address',  async function (req) {
 //分组器调用
 //测试   curl -X POST -H "Content-Type: application/json" -k -d '{"B_WT4_V1_ID": 54950188,"DISEASE_CODE": "Z51.103","AGE": 62,"GENDER": "男","SF0100": -1,"SF0102": -1,"SF0104": -1,"SF0108": 1,"ACCTUAL_DAYS": 1,"TOTAL_EXPENSE": 12538.92,"diags_code": ["C20xx01","C77.901","C78.001","C78.701","Z22.501"],"opers_code": ["89.54001","99.25020"]}' http://127.0.0.1:4096/api/dapps/dapp/drgserver
 app.route.post('/drgserver',  async function (req) {
-   //作为调用分组使用
-   // data = JSON.stringify(req.query);
-   // var response = await request
-   //                       .post('139.129.165.56:3002/comp_drg')
-   //                       .send(req.query)
-   //                       .set('Content-Type','application/json')
-   //dapp转账,目前存余额不足情况
-  var fee = String(0.1 * 100000000);
-  var type = 3;
-  var options = {fee: fee, type: type, args: '["CCTime.XCT", "10", "'+req.query["Key"]+'"]'};
-  var secret = "elite brush pave enable history risk ankle shrimp debate witness ski trend";
-  var transaction = AschJS.dapp.createInnerTransaction(options, secret);
+  //作为调用分组使用
   var response = await request
-    .put('127.0.0.1:4096/api/dapps/5c4340ce57d698398a921a45743e24d2b6f81895da2db645844167680d9805c9/transactions/signed')
-    .set('Content-Type','application/json')
-    .set('magic', '594fe0f3')
-    .set('version','')
-    .set('accept', 'json')
-    .send({"transaction": transaction})
-    .set('Content-Type','application/json')
-  return {reports: response}
+                         .post('139.129.165.56:3001/comp_drg')
+                         .send(req.query)
+                         .set('Content-Type','application/json')
+  //判定分组是否失败
+  if(response['text'].error){
+    return {info: "分组失败,没有产生交易", success: false}
+  }else{
+    //费用
+    const fee = String(0.1 * 100000000);
+    const type = 3;
+    //每次交易1,A8QCwz5Vs77UGX9YqBg9kJ6AZmsXQBC8vj为创世账号地址
+    const options = {fee: fee, type: type, args: '["XAS", "100000000", "A8QCwz5Vs77UGX9YqBg9kJ6AZmsXQBC8vj"]'};
+    //调用分组传来的账号
+    const secret = req.query["secret"];
+    const transaction = AschJS.dapp.createInnerTransaction(options, secret);
+    //调用dapp内交易
+    var response = await request
+                .put('127.0.0.1:4096/api/dapps/27d0d9176c51e3b9406e39a88b4fec3a71af06bbfbda4964579d2e38b58019d1/transactions/signed')
+                .set('Content-Type','application/json')
+                .set('magic', '594fe0f3')
+                .set('version','')
+                .set('accept', 'json')
+                .send({"transaction": transaction})
+                .set('Content-Type','application/json')
+    if(JSON.parse(response['text']).success){
+      return {info: "分组成功,交易成功", success: true}
+    }else{
+      return {info: "分组成功,交易失败,余额不足", success: false}
+    }
+  }
 })
