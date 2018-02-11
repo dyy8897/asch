6d5091a5137616672f908da371b5571b3cffc55d
duanzhichao <duanzhichao2008@gmail.com>
2018-02-11 13:06:43 +0800

    增加通过公钥获取地址方法

--- dy-dapps/common/interface/helloworld.js
+++ dy-dapps/common/interface/helloworld.js
@@ -17,6 +17,16 @@ app.route.post('/sleep',  async function (req, cb) {
   }
   await new Promise(resolve => setTimeout(resolve, Math.floor(Number(req.query.seconds) * 1000)))
 })
+app.route.get('/address',  async function (req) {
+  const secret = req.query['secret']
+  const keys = AschJS.crypto.getKeys(secret)
+  // 通过公钥获得地址
+  const publicKey = keys.publicKey
+  const privateKey = keys.privateKey
+  const address =  AschJS.crypto.getAddress(publicKey)
+  return { message: address}
+})
+
 //分组器调用
 //测试   curl -X POST -H "Content-Type: application/json" -k -d '{"B_WT4_V1_ID": 54950188,"DISEASE_CODE": "Z51.103","AGE": 62,"GENDER": "男","SF0100": -1,"SF0102": -1,"SF0104": -1,"SF0108": 1,"ACCTUAL_DAYS": 1,"TOTAL_EXPENSE": 12538.92,"diags_code": ["C20xx01","C77.901","C78.001","C78.701","Z22.501"],"opers_code": ["89.54001","99.25020"]}' http://127.0.0.1:4096/api/dapps/dapp/drgserver
 app.route.post('/drgserver',  async function (req) {
