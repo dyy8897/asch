9a9d2ccd5abf41aa118d893af08cd7b5a902b984
duanzhichao <duanzhichao2008@gmail.com>
2018-02-12 09:54:10 +0800

    修改变量定义

--- dy-dapps/common/interface/helloworld.js
+++ dy-dapps/common/interface/helloworld.js
@@ -31,7 +31,7 @@ app.route.get('/address',  async function (req) {
 //测试   curl -X POST -H "Content-Type: application/json" -k -d '{"B_WT4_V1_ID": 54950188,"DISEASE_CODE": "Z51.103","AGE": 62,"GENDER": "男","SF0100": -1,"SF0102": -1,"SF0104": -1,"SF0108": 1,"ACCTUAL_DAYS": 1,"TOTAL_EXPENSE": 12538.92,"diags_code": ["C20xx01","C77.901","C78.001","C78.701","Z22.501"],"opers_code": ["89.54001","99.25020"]}' http://127.0.0.1:4096/api/dapps/dapp/drgserver
 app.route.post('/drgserver',  async function (req) {
   //作为调用分组使用
-  var response = await request
+  let response = await request
                          .post('139.129.165.56:3001/comp_drg')
                          .send(req.query)
                          .set('Content-Type','application/json')
@@ -48,7 +48,7 @@ app.route.post('/drgserver',  async function (req) {
     const secret = req.query["secret"];
     const transaction = AschJS.dapp.createInnerTransaction(options, secret);
     //调用dapp内交易
-    var response = await request
+    let response = await request
                 .put('127.0.0.1:4096/api/dapps/27d0d9176c51e3b9406e39a88b4fec3a71af06bbfbda4964579d2e38b58019d1/transactions/signed')
                 .set('Content-Type','application/json')
                 .set('magic', '594fe0f3')
